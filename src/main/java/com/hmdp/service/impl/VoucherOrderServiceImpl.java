bb3451e9cc88fc9279d5cf164445b3e8f9f3c627
Xancel <108331256+XancelZC@users.noreply.github.com>
2025-05-27 20:12:28 +0800

    完成悲观锁解决一人一单问题 锁粒度选择是按用户ID加锁，所以要使用intern方法（intern方法让相同值的字符串指向内存中的同一对象） 代理对象是为了保证事务和锁的正确顺序，也就是： 获取锁->通过代理调用方法->代理开启事务->执行业务->事务提交/回滚->释放锁

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -9,6 +9,7 @@ import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisIdWorker;
 import com.hmdp.utils.UserHolder;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,7 +33,6 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     private RedisIdWorker redisIdWorker;
 
     @Override
-    @Transactional
     public Result seckillVoucher(Long voucherId) {
         // 1 查询优惠券
         SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
@@ -51,11 +51,32 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         if (stock < 1){
             return Result.fail("库存不足");
         }
+
+        Long userId = UserHolder.getUser().getId();
+        synchronized (userId.toString().intern()) {
+            //获取代理对象（事务）
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        }
+    }
+
+    @Transactional
+    public Result createVoucherOrder(Long voucherId) {
+        // add 增加一人一单逻辑
+        // a.1获取用户id
+        Long userId = UserHolder.getUser().getId();
+        synchronized (userId.toString().intern()){
+        int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+        // a.2判断是否存在
+        if (count > 0){
+            return Result.fail("用户已经购买过了");
+        }
+
         // 5 扣减库存
         boolean success = seckillVoucherService
                 .update()
                 .setSql("stock = stock-1")
-                .eq("voucher_id", voucherId).gt("stock",0)  //乐观锁解决超卖问题
+                .eq("voucher_id", voucherId).gt("stock",0)  //乐观锁解决超卖问题 用于更新业务（此处为扣减库存）
                 .update();
         if (!success){
             return Result.fail("库存不足");
