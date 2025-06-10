97e60eb511b3e6eb17e49250dc00483fb5bbd650
Xancel <108331256+XancelZC@users.noreply.github.com>
2025-06-10 19:53:45 +0800

    完成关注和取消关注功能

--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,10 +1,16 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -17,4 +23,45 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result follow(Long followUserId, Boolean isFollow) {
+        // 1 获取登录用户
+        Long userId = UserHolder.getUser().getId();
+        String key = "follows:" + userId;
+        // 2 判断是关注还是取关
+        if (isFollow) {
+            // 2.1 关注 新增数据
+            Follow follow = new Follow();
+            follow.setUserId(userId);
+            follow.setFollowUserId(followUserId);
+            boolean isSuccess = save(follow);
+            if (isSuccess){
+                // 把关注用户的id 放入redis的set集合中
+                stringRedisTemplate.opsForSet().add(key,followUserId.toString());
+            }
+        }else {
+            // 2.2 取关 删除数据
+            remove(new QueryWrapper<Follow>()
+                    .eq(("user_id"),userId)
+                    .eq("follow_user_id", followUserId));
+            // 把关注用户的id从redis中移除
+            stringRedisTemplate.opsForSet().remove(key,followUserId.toString());
+        }
+        return Result.ok();
+    }
+
+    @Override
+    public Result isFollow(Long followUserId) {
+        // 1 获取登录用户
+        Long userId = UserHolder.getUser().getId();
+        // 2 查询是否关注
+        Integer count = query().eq("user_id", userId)
+                .eq("follow_user_id", followUserId)
+                .count();
+        // 3 判断
+        return Result.ok(count>0);
+    }
 }
